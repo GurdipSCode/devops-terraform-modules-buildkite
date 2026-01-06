e6514a314aa4c9392d30ace37bfef6153e174bfd
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2026-01-06 18:40:07 +0000

    Update python_pipelines.tf

--- modules/pipelines/python_pipelines.tf
+++ modules/pipelines/python_pipelines.tf
@@ -9,3 +9,27 @@ resource "buildkite_pipeline" "this" {
   branch_configuration = each.value.branch
   tags                 = each.value.tags
 }
+
+resource "buildkite_pipeline" "opentofu_pipelines" {
+  for_each = var.opentofu_pipelines
+
+  name        = each.key
+  repository  = each.value.repository
+  description = each.value.description
+
+  pipeline_file        = each.value.pipeline_file
+  branch_configuration = each.value.branch
+  tags                 = each.value.tags
+}
+
+resource "buildkite_pipeline" "opa_pipelines" {
+  for_each = var.opa_pipelines
+
+  name        = each.key
+  repository  = each.value.repository
+  description = each.value.description
+
+  pipeline_file        = each.value.pipeline_file
+  branch_configuration = each.value.branch
+  tags                 = each.value.tags
+}
